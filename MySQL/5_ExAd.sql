655776b33a8da31a089c1cb9167ad4c01ccb201c
HellBz <stefan-koegler@freenet.de>
2017-05-06 12:10:45 +0200

    Add ExAdClient
    Source: https://github.com/Bjanski/ExAd

---
+++ MySQL/5_ExAd.sql
@@ -0,0 +1,2 @@
+	ALTER TABLE `vehicle` ADD `territory_id` INT(11) UNSIGNED NULL DEFAULT NULL;
+	ALTER TABLE `vehicle` ADD CONSTRAINT `vehicle_ibfk_2` FOREIGN KEY (`territory_id`) REFERENCES `territory`(`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
